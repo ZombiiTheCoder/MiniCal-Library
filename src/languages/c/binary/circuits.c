ece36727f60898a8cccbaa3eac2c28d7cca26789
Zombi <zombeifedgaming@outlook.com>
2023-04-30 01:37:32 -0500

    Add C scaffold

---
+++ src/languages/c/binary/circuits.c
@@ -0,0 +1,13 @@
+#include "../include/circuits.h"
+
+char* adder(char* a, char* b, bool carry){
+
+
+
+}
+
+char* subtractor(char* a, char* b){
+
+
+
+}
---
+++ src/languages/c/binary/converter.c
@@ -0,0 +1,37 @@
+#include "../include/converter.h"
+
+bool CharToBool(char a){
+
+
+
+}
+
+char BoolToChar(bool a){
+
+
+
+}
+
+char* ConstructCharList(bool* a){
+
+
+
+}
+
+bool* DeconstructCharList(char* a){
+
+
+
+}
+
+bool* ReverseBoolList(bool* a){
+
+
+
+}
+
+char* ExtendBinary(char* a){
+
+
+    
+}
